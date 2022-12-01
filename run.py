f98e13a90c68994070ed6bdf14e0ad726f91fc6f
Mahmoud Gody <mahmou.m.gody@gmail.com>
2022-12-01 11:22:00 +0200

    Add : pitch, energy and duration controls to synthesize

--- run.py
+++ run.py
@@ -35,5 +35,6 @@ class TextToSpeech:
             self.configs, self.vocoder_config_path, self.speaker_pre_trained_path,Model_URL
         )
 
-    def synthesize(self, text, bw=False, apply_tshkeel=False):
-        infer_tts(text, self.model, self.vocoder, self.configs, bw=bw, apply_tshkeel=apply_tshkeel)
+    def synthesize(self, text, bw=False, apply_tshkeel=False, controls=(1.0, 1.0, 1.0)):
+        p_control, e_control, d_control = controls
+        infer_tts(text, self.model, self.vocoder, self.configs, bw=bw, apply_tshkeel=apply_tshkeel, pitch_control=p_control, energy_control=e_control, duration_control=d_control)
