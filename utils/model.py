5bd45c7dc511e96972a17daaa26eadfbde629f8c
Mahmoud Amro Mohamed | Ejada Systems Ltd <mahmou.m.gody@gmail.com>
2022-12-14 11:15:46 +0200

    Add: revert changes in get_vocoder

--- utils/model.py
+++ utils/model.py
@@ -56,54 +56,30 @@ def get_param_num(model):
     return num_param
 
 
-# def get_vocoder(config, device):
-#     name = config["vocoder"]["model"]
-#     speaker = config["vocoder"]["speaker"]
-
-#     if name == "MelGAN":
-#         if speaker == "LJSpeech":
-#             vocoder = torch.hub.load(
-#                 "descriptinc/melgan-neurips", "load_melgan", "linda_johnson"
-#             )
-#         elif speaker == "universal":
-#             vocoder = torch.hub.load(
-#                 "descriptinc/melgan-neurips", "load_melgan", "multi_speaker"
-#             )
-#         vocoder.mel2wav.eval()
-#         vocoder.mel2wav.to(device)
-#     elif name == "HiFi-GAN":
-#         with open("hifigan/config.json", "r") as f:
-#             config = json.load(f)
-#         config = hifigan.AttrDict(config)
-#         vocoder = hifigan.Generator(config)
-#         if speaker == "LJSpeech":
-#             ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar")
-#         elif speaker == "universal":
-#             ckpt = torch.load("hifigan/generator_universal.pth.tar")
-#         vocoder.load_state_dict(ckpt["generator"])
-#         vocoder.eval()
-#         vocoder.remove_weight_norm()
-#         vocoder.to(device)
-
-#     return vocoder
-
-def get_vocoder(config, device, vocoder_config_path=None, speaker_pre_trained_path=None):
+def get_vocoder(config, device):
     name = config["vocoder"]["model"]
     speaker = config["vocoder"]["speaker"]
 
     if name == "MelGAN":
         if speaker == "LJSpeech":
-            vocoder = torch.hub.load("descriptinc/melgan-neurips", "load_melgan", "linda_johnson")
+            vocoder = torch.hub.load(
+                "descriptinc/melgan-neurips", "load_melgan", "linda_johnson"
+            )
         elif speaker == "universal":
-            vocoder = torch.hub.load("descriptinc/melgan-neurips", "load_melgan", "multi_speaker")
+            vocoder = torch.hub.load(
+                "descriptinc/melgan-neurips", "load_melgan", "multi_speaker"
+            )
         vocoder.mel2wav.eval()
         vocoder.mel2wav.to(device)
     elif name == "HiFi-GAN":
-        with open(vocoder_config_path, "r") as f:
+        with open("hifigan/config.json", "r") as f:
             config = json.load(f)
         config = hifigan.AttrDict(config)
         vocoder = hifigan.Generator(config)
-        ckpt = torch.load(speaker_pre_trained_path, map_location=torch.device("cpu"))
+        if speaker == "LJSpeech":
+            ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar")
+        elif speaker == "universal":
+            ckpt = torch.load("hifigan/generator_universal.pth.tar")
         vocoder.load_state_dict(ckpt["generator"])
         vocoder.eval()
         vocoder.remove_weight_norm()
